d2ef7430419ed003c49846c398093eec6a9f48fe
NinjaWu2000 <Spinjitzu_Meister_28@outlook.com>
2025-02-18 17:34:26 +0100

    added suport for diffrent behaviors

--- boards/shields/blockytype/behaviors/mod_morph_generator.py
+++ boards/shields/blockytype/behaviors/mod_morph_generator.py
@@ -1,27 +1,40 @@
 mod_list = []
 normal_list = []
 modified_list = []
+normal_behavior = ""
+modified_behavior = ""
+
+
+def clear_last_entry():
+    global normal_behavior, modified_behavior
+    lists = [mod_list, normal_list, modified_list]
+    lenght = len(lists[0])
+    for l in lists:
+        if len(l) == lenght:
+            l.pop()
+    if lenght == 1:
+        normal_behavior = ""
+        modified_behavior = ""
 
 
 while True:
     if mod_list == []:
+        normal_behavior = input("normal behavior? ").lower()
+        modified_behavior = input("modified behavior? ").lower()
         mod_list.append(input("mod? "))
-        if mod_list[-1] == "":
-            mod_list.pop()
+        if mod_list[-1] == "" or normal_behavior == "" or modified_behavior == "":
+            clear_last_entry()
             continue
     else:
         mod_list.append(mod_list[0])
         print(f"mod? {mod_list[0]}")
-    normal_list.append(input("normal? "))
+    normal_list.append(input("normal key? "))
     if normal_list[-1] == "":
-        mod_list.pop()
-        normal_list.pop()
+        clear_last_entry()
         continue
-    modified_list.append(input("modified? "))
+    modified_list.append(input("modified key? "))
     if modified_list[-1] == "":
-        mod_list.pop()
-        normal_list.pop()
-        modified_list.pop()
+        clear_last_entry()
         continue
     stop = input("[continue]/print? ")
     if stop == "" or stop == "continue":
@@ -44,7 +57,9 @@ while True:
             )
             print(f'    compatible = "zmk,behavior-mod-morph";')
             print(f"    #binding-cells = <0>;")
-            print(f"    bindings = <&kp {normal.upper()}>, <&kp {modified.upper()}>;")
+            print(
+                f"    bindings = <&{normal_behavior} {normal.upper()}>, <&{modified_behavior} {modified.upper()}>;"
+            )
             print(f"    mods = <(MOD_L{mod.upper()} | MOD_R{mod.upper()})>;")
             print(f"{help[1]};")
         break
